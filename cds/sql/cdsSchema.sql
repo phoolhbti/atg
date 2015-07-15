fa0dc8e114526f12b5f9c9205d6925b8f810fa27
Phool Chandra <phoolhbti@gmail.com>
2015-07-15 14:13:53 +0530

    Update cdsSchema.sql

--- cds/sql/cdsSchema.sql
+++ cds/sql/cdsSchema.sql
@@ -1,60 +1,56 @@
-CREATE TABLE cds_product (
-        product_id                     INT(11),
-		name							VARCHAR(255),
-        Manufacturer                 VARCHAR(100),
-        Short description            VARCHAR (255),
-		Long description          	VARCHAR(255),
+		CREATE TABLE cds_category (
+		category_id 			INT(11) not null,
+		name 					VARCHAR(255),
+		details					TEXT,
+		primary key(category_id)
+		);
+
+		CREATE TABLE cds_product (
+        product_id                  INT(11) not null,
+		name						VARCHAR(255),
+        Manufacturer                VARCHAR(100),
+        Shortdescription            VARCHAR (255),
+		Longdescription          	VARCHAR(255),
         image                   	VARCHAR(255),
         Price						VARCHAR(255),
-        Review						VARCHAR(255),
-		category_id					INT(11),
+        Review						VARCHAR(255),		
 		quantity					VARCHAR(255),
         primary key(product_id)
-        );
-		
-		
+        );		
+		CREATE TABLE cds_category_products (
+        category_id                INT(11)     not null references cds_category(category_id),
+        product_list               INT(11)     not null references cds_product(product_id),
+        primary key(category_id, product_list)
+);
 		CREATE TABLE cds_users (
-        user_id                 VARCHAR(255) ,
-        firstname 				VARCHAR(255),
-        middlename				VARCHAR(255),
-		lastname				VARCHAR(255),
-		email        			VARCHAR(255),	
-		password				VARCHAR(255),
+       	user_id                 VARCHAR(255) references dps_user(id),
+        info 					VARCHAR(255),
 		address					VARCHAR(255),	
 		phone					VARCHAR(255),
-		type					VARCHAR(255),
+		types					VARCHAR(255),
 		primary key(user_id)
 		);
 		
-		CREATE TABLE cds_category (
-		id 						INT(11)
-		name 					VARCHAR(255),
-		details					TEXT,
-		primary key(id)
-		);
+		CREATE TABLE cds_category_product(
+    	 category_id    VARCHAR(32)  not null references cds_category(category_id),
+     	 product_id   VARCHAR(32)  not null,
+    	 primary key(category_id, product_id)
+           );
 		
 		CREATE TABLE cds_orders (
-		order_id				INT(11),
-		user_id					INT(11),
+		order_id				INT(11) references cds_product(product_id),
+		user_id					INT(11) references dps_user(id),
 		shipping_address		VARCHAR(255),
 		shipping_date			INT(11),
 		shipping_status			VARCHAR(255),
 		created_at 				INT(11),
+		quantity				INT(11),
 		primary key(order_id)
 		);
 		
-		CREATE TABLE cds_order_details (
-		order_details_id 		INT(11),
-		order_id				INT(11),
-		product_id				INT(11),
-		quantity				INT(11),
-		primary key(order_details_id)
-		);
-		
-		
 		CREATE TABLE cds_sales (
-		sales_id				INT(11),
+		sales_id				INT(11) references cds_orders(order_id),
 		order_id				INT(11),
-		sales_amount			INT(11),
+		sales_amount			INT(11),	
 		primary key(sales_id)
 		);
