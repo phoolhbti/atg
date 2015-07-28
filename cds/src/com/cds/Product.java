7223fcc761b7dd72945634a933b14f67d773560a
Phool Chandra <phoolhbti@gmail.com>
2015-07-28 13:48:06 +0530

    Create Product.java

---
+++ cds/src/com/cds/Product.java
@@ -0,0 +1,61 @@
+package com.cds;
+
+public class Product {
+private String	productId;	
+private String	manufacturer;
+private String	shortdescription;
+private String	longdescription;
+private String	review;
+private String	price;
+private String	image;
+private String	name;
+public String getManufacturer() {
+	return manufacturer;
+}
+public void setManufacturer(String manufacturer) {
+	this.manufacturer = manufacturer;
+}
+public String getShortdescription() {
+	return shortdescription;
+}
+public void setShortdescription(String shortdescription) {
+	this.shortdescription = shortdescription;
+}
+public String getLongdescription() {
+	return longdescription;
+}
+public void setLongdescription(String longdescription) {
+	this.longdescription = longdescription;
+}
+public String getReview() {
+	return review;
+}
+public void setReview(String review) {
+	this.review = review;
+}
+public String getPrice() {
+	return price;
+}
+public void setPrice(String price) {
+	this.price = price;
+}
+public String getImage() {
+	return image;
+}
+public void setImage(String image) {
+	this.image = image;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getProductId() {
+	return productId;
+}
+public void setProductId(String productId) {
+	this.productId = productId;
+}
+
+}
